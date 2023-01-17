731003add0e22359ca8ac39d4784b1b8a843934c
Stefan <lesternuhkyare@gmail.com>
2023-01-17 01:36:33 -0500

    .

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,15 +1,14 @@
 import React from "react";
 import styles from "./AboutMe.module.scss";
 import selfie from "../assets/aistefan.jpg";
-import Typist from 'react-typist-component';
-import hand from "../assets/waving.png";
+import Typist from "react-typist-component";
 
 const AboutMe = () => {
   return (
     <section id="aboutme">
       <div className={styles.container}>
-      <div className={styles["right-container"]}>
-          <img src={selfie} className={styles.selfie}></img>
+        <div className={styles["right-container"]}>
+          <img src={selfie} className={styles.selfie} alt="Me"></img>
         </div>
         <div className={styles.abouttext}>
           <div className={styles.info}>
@@ -22,14 +21,14 @@ const AboutMe = () => {
                 <Typist.Delay ms={2000} />
                 <span className={styles.span}>&lt;h1&gt;</span>
                 <h2 className={styles.h2}>
-                  Hello! <br/>
+                  Hello! <br />
                   I'm Stefan. <br /> Developer from Hamilton, Ontario.
                 </h2>
                 <span className={styles.span}>&lt;/h1&gt;</span>
               </Typist>
             </div>
             <Typist>
-            <Typist.Delay ms={1000} />
+              <Typist.Delay ms={1000} />
               <span className={styles.span}>&lt;/body&gt;</span>
             </Typist>
           </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,12 +6,12 @@ import resume from "../assets/Stefan-Parenta-Resume.pdf";
 
 const NavBar = () => {
   return (
-    <nav
-      className={styles.nav}
-    >
-      <ul>
+    <nav className={styles.nav}>
+      <ul className={styles.left}>
         <li>
-          <h2 className={styles.h2}><a href="#top">Stefan Parenta</a></h2>
+          <h2 className={styles.h2}>
+            <a href="#top">Stefan Parenta</a>
+          </h2>
         </li>
         <li className={styles.links}>
           <a href="#top">Home</a>
@@ -22,13 +22,33 @@ const NavBar = () => {
         <li className={styles.links}>
           <a href="#contactme">Contact Me</a>
         </li>
-        <li className={styles.resume}><a download href={resume}>Resume</a></li>
-        <li className={styles.right}>
-            <a className={styles.jump} href="https://www.github.com/stefan-parenta" target="_blank" rel="noreferrer" ><DiGithubBadge className={styles.icon} size={30}/></a>
+        <li className={styles.resume}>
+          <a download href={resume} type="application/pdf">
+            Resume
+          </a>
         </li>
-        <li>
-          <a className={styles.jump} href="https://www.linkedin.com/in/stefan-parenta-59232b262/" target="_blank" rel="noreferrer" ><SiLinkedin className={styles.icon} size={20}/></a>
-        </li>
+        <ul className={styles.right}>
+          <li>
+            <a
+              className={styles.jump}
+              href="https://www.github.com/stefan-parenta"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <DiGithubBadge className={styles.icon} size={30} />
+            </a>
+          </li>
+          <li>
+            <a
+              className={styles.jump}
+              href="https://www.linkedin.com/in/stefan-parenta-59232b262/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <SiLinkedin className={styles.icon} size={20} />
+            </a>
+          </li>
+        </ul>
       </ul>
     </nav>
   );
