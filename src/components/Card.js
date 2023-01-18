dd26acd2fffd4e39126babdbbc7e20f787804a9f
Stefan <lesternuhkyare@gmail.com>
2023-01-17 23:08:27 -0500

    m

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from "react";
 import styles from "./Card.module.scss";
 
 const Card = (props) => {
   return (
     <div className={styles.card}>
-        <div className={styles['card-body']}>
-            <img className={styles['card-img']} src={props.img} />
-            <h2 className={styles['card-title']}>{props.title}</h2>
-            <p className={styles['card-desc']}>{props.desc}</p>
-        </div>
-        <a className={styles['card-btn']} href={props.href} target="_blank">{props.btn}</a>
+      <div className={styles["card-body"]}>
+        <img className={styles["card-img"]} src={props.img} alt="Card" />
+        <h2 className={styles["card-title"]}>{props.title}</h2>
+        <p className={styles["card-desc"]}>{props.desc}</p>
+      </div>
+      <a
+        className={styles["card-btn"]}
+        href={props.href}
+        target="_blank"
+        rel="noreferrer"
+      >
+        {props.btn}
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
